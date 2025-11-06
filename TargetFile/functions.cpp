623254b6bc73f1b3373f620e4c06faa276f9d4e5
Changqing Jing <changqing.jing@bmw.com>
2025-11-06 10:43:01 +0800

    update demo

--- TargetFile/functions.cpp
+++ TargetFile/functions.cpp
@@ -2,13 +2,14 @@ class C1 {
 public:
   int foo() {
     int f11 = 1;
-    return f11;
+    return f11 + a1;
   }
 
-  static int goo() {
+  static int goo(C1 &gp1, C1 *gp2) {
     int g11 = 2;
-    return g11;
+    return g11 + gp1.a1 + gp2->a1;
   }
+  int a1{};
 };
 
 namespace N1 {
@@ -25,7 +26,9 @@ void zoo() {
 int main() {
   C1 c1;
   int f12 = c1.foo();
-  int g12 = C1::goo();
+  C1 g1{};
+  C1 g2{};
+  int g12 = C1::goo(g1, &g2);
 
   N1::C2 c2;
   c2.yoo();
